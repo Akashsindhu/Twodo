6a43824de34a70cc264e84bfb67db320cae144a0
Danny Giap <dannygiap@Dannys-MacBook-Pro.local>
2019-11-04 01:06:23 -0800

    added sign-in buttion and login page

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,14 @@
 from flask import render_template
 from app import myApp
+from app.forms import Login
 
 @myApp.route('/')
 def front():
 	return render_template('index.html')
-		
+
+@myApp.route('/login')
+def login():
+	form = Login()
+	if form.validate_on_submit():
+		pass
+	return render_template('login.html', form=form)
