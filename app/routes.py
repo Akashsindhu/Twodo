76d10293cfd533c58521f93a42ef43dd8cb32409
Danny Giap <dannygiap@Dannys-MacBook-Pro.local>
2019-11-30 01:00:53 -0800

    added more comments

--- app/routes.py
+++ app/routes.py
@@ -15,7 +15,9 @@ from werkzeug.urls import url_parse
 @app.route('/index')
 @login_required 
 def index():
+    #grab the current user by filtering through all the users
     user = User.query.filter_by(username=current_user.username).first()
+    #grab the list of projects that the current user has
     projects = user.projects.all()
     return render_template('index.html', title='Home', projects=projects)
 
@@ -60,24 +62,33 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
+#prints the list of tasks within a project
 @app.route('/project/int:<id>')
 def project(id):
+    #grab current project. Filters the list using the unique id belonging to the project. 
+    #refer to models.py to see all the attributes of each database
     current_project = current_user.projects.filter_by(id=id).first()
+    #separate incomplete and complete projects using the 'complete' attribute that is unique to each todo item
     incomplete = current_project.todos.filter_by(complete=False).all()
     complete = current_project.todos.filter_by(complete=True).all()
+    #pass in the incomplete and complete items into the render function
     return render_template('project.html', project=current_project, complete=complete, incomplete=incomplete)
     #return '<h1>{}</h1>'.format(current_project.name)
 
 #routes to a new page that grabs input from the user
 @app.route('/create') #create tasks
 def create():
+    #create.html is the page that takes in the project name and routes to /addProject
     return render_template('create.html')
 
 #/create redirects user to this url. A new project is created and added to the database in this url    
 @app.route('/addProject', methods=['POST']) #create tasks
 def addProject():
+    #create a new Project using the name inputted in create.html
     project = Project(name=request.form['Project name'])
+    #add to database
     db.session.add(project)
+    #update list of contributors for the Project
     project.contributors.append(current_user)
     db.session.commit()
     return redirect(url_for('index'))
@@ -85,7 +96,9 @@ def addProject():
 #clicking the add task button will route you here. A task is created and added to the database here
 @app.route('/addTask/int:<id>', methods=['POST']) #create tasks
 def addTask(id):
+    #grab current project by filter by id
     current_project = current_user.projects.filter_by(id=id).first()
+    #create a new Todo. This Todo will be added to the database. The text is grabbed from project.html 
     todo = Todo(task=request.form['task'], project=current_project)
     db.session.add(todo)
     db.session.commit()
@@ -99,9 +112,9 @@ def deleteTask(project_id, task_id):
     task = current_project.todos.filter_by(id=task_id).first()
     db.session.delete(task)
     db.session.commit()
-    #return '<h1>{}</h1>'.format(task.task)
     return redirect(url_for('project', id=current_project.id))
 
+#marks a task as complete by changing the 'complete' attribute to True
 @app.route('/complete/int:<project_id>/int:<task_id>', methods=['POST'])
 def complete(project_id, task_id):
     current_project = current_user.projects.filter_by(id=project_id).first()
